015ada26ea3657d6fe2a0d3181d52186b46a4fab
nspool <nspool@icloud.com>
2017-03-04 20:55:50 +1100

    Add Mat and SparseMat examples

--- Hello-OpenCV/main.cpp
+++ Hello-OpenCV/main.cpp
@@ -22,24 +22,56 @@
 //cv::RotatedRect
 //cv::Scalar a four-dimensional point class, element-wise multiplication, quaternion conjection
 
-int main(int argc, const char * argv[]) {
-
-    cv::Mat img = cv::imread(argv[1], -1);
-    if(img.empty()){
-        return -1;
+void createSparse() {
+    int size[] = {10,10};
+    cv::SparseMat sm(2,size, CV_32F);
+    
+    for(int i=0; i<10; i++) {
+        int idx[2];
+        idx[0] = size[0] * rand();
+        idx[1] = size[1] * rand();
+        sm.ref<float>(idx) += 1.0f;
     }
     
-    cv::namedWindow("Example", cv::WINDOW_AUTOSIZE);
-    
-    cv::Mat out;
-    
-    cv::imshow("Example", img);
-    cv::waitKey();
+    // cv::SparseMatConstIterator_<float> it = sm.begin<float>();
+    // cv::SparseMatConstIterator_<float> it_end = sm.end<float>();
+    auto it = sm.begin<float>();
+    auto it_end = sm.end();
+    for(;it != it_end; ++it) {
+        const cv::SparseMat::Node* node = it.node();
+        printf("(%3d,%3d) %f\n", node->idx[0],node->idx[1], *it);
+    }
+}
 
-    cv::pyrDown(img,out);
-    cv::imshow("Example", out);
-    cv::waitKey();
-    cv::destroyWindow("Example");
+
+void createMat() {
+    cv::Mat m;
+    m.create(3, 10, CV_32FC3); // Create data area for 3 rows and 10 columns of 3-channel 32-bit floats
+    m.setTo(cv::Scalar(1.0f, 0.0f, 1.0f)); // Set the value of 1st and 3rd channels to 1.0, 2nd channel to 0
+    // Equivalent to:
+    cv::Mat m2(3,10,CV_32FC3,cv::Scalar(1.0f, 0.0f, 1.0f));
+}
+
+int main(int argc, const char * argv[]) {
     
+    createSparse();
+
+//    cv::Mat img = cv::imread(argv[1], -1);
+//    if(img.empty()){
+//        return -1;
+//    }
+//    
+//    cv::namedWindow("Example", cv::WINDOW_AUTOSIZE);
+//    
+//    cv::Mat out;
+//    
+//    cv::imshow("Example", img);
+//    cv::waitKey();
+//
+//    cv::pyrDown(img,out);
+//    cv::imshow("Example", out);
+//    cv::waitKey();
+//    cv::destroyWindow("Example");
+//    
     return 0;
 }
