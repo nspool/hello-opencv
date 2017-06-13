a51e4e35e4608a1357d37f98040555d828c7fff9
nspool <nspool@gmx.com>
2017-06-13 21:38:26 +1000

    Working BackProjection example (finally!)

--- Hello-OpenCV/ImageProcessor.cpp
+++ Hello-OpenCV/ImageProcessor.cpp
@@ -1,9 +1,9 @@
 //
-//  ImageProcessor.cpp
-//  Hello-OpenCV
+// ImageProcessor.cpp
+// Hello-OpenCV
 //
-//  Created by nsp on 23/5/17.
-//  Copyright © 2017 nspool. All rights reserved.
+// Created by nsp on 23/5/17.
+// Copyright © 2017 nspool. All rights reserved.
 //
 
 #include <vector>
@@ -52,19 +52,15 @@ cv::Mat ImageProcessor::templateMatching() {
 
 // TODO: Example of using the Earth Mover's Distance to compare histograms
 void ImageProcessor::histogramEMD() {
-//    
+    
     cv::Mat a = cv::imread("a.jpg");
     cv::Mat b = cv::imread("a.jpg");
-//    = cv::EMD(a, b, 2, nullptr, 0, nullptr);
-//
     
-    double distance = cv::EMDL1(a,b);;
+    double distance = cv::EMDL1(a,b);
     
     printf("Earth Mover's Distance: %f\n", distance);
 }
 
-
-// TODO: Back Projection example
 cv::Mat ImageProcessor::backProjection() {
     
     std::vector<cv::Mat> images;
@@ -74,71 +70,14 @@ cv::Mat ImageProcessor::backProjection() {
     // Canvas contains the color of the background canvas
     cv::Mat canvas = cv::imread("canvas.png");
     
-    std::vector<int> channels;
-    channels.push_back(0);
-    channels.push_back(1);
-    channels.push_back(2);
-
-    std::vector<int> histSize;
-    histSize.push_back(16);
-    histSize.push_back(16);
-    histSize.push_back(16);
+    cv::Mat output, hist;
+    int histSize[] = {16, 16, 16};
+    int channels[] = {0, 1, 2};
+    const float hRange[] = {0.f, 256.f};
+    const float* ranges[] = {hRange, hRange, hRange};
     
-////    std::vector<float> ranges;
-//    cv::Mat backProject;
-//
-//
-//
-////    ranges.push_back(10);
-////    channels.push_back(canvas.channels());
-//    
-//    cv::Mat output, hist;
-//    int histSize[] = {16, 16, 16};
-////    int channels[] = {0, 1, 2};
-//    const float hRange[] = {0.f, 256.f};
-//    const float* ranges[] = {hRange, hRange, hRange};
+    cv::calcHist(&canvas, 1, channels, cv::noArray(), hist, 2, histSize, ranges);
+    cv::calcBackProject(&rabbit, 1, channels, hist, output, ranges);
     
-    std::vector<float> ranges;
-    ranges.push_back(0);
-    ranges.push_back(0);
-    ranges.push_back(0);
-    ranges.push_back(0);
-
-    cv::Mat hist;
-    cv::calcHist({canvas}, channels, cv::noArray(), hist, histSize, ranges);
-//
-////    CV_EXPORTS_W void calcBackProject( InputArrayOfArrays images, const std::vector<int>& channels,
-////                                      InputArray hist, OutputArray dst,
-////                                      const std::vector<float>& ranges,
-////                                      double scale );
-////    
-
-    cv::Mat output;
-    cv::calcBackProject({rabbit}, channels, hist, output, ranges, 1.0);
     return output;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
