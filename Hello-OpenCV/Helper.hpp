bf86fcf9d0edfdc6750e2701e3cc4ee8131a2395
nspool <nspool@gmx.com>
2017-09-15 20:10:27 +1000

    Refactor other examples into Helper class

---
+++ Hello-OpenCV/Helper.hpp
@@ -0,0 +1,22 @@
+//
+//  Helper.hpp
+//  Hello-OpenCV
+//
+//  Created by nsp on 15/9/17.
+//  Copyright © 2017 nspool. All rights reserved.
+//
+
+#ifndef Helper_hpp
+#define Helper_hpp
+
+#include <stdio.h>
+#include "main.hpp"
+
+class Helper {
+public:
+    void createSparse();
+    void createMat();
+    void scalarPlay();
+};
+
+#endif /* Helper_hpp */
